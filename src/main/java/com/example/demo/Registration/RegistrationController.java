60a8bf4caad10c8ee51dd31c549ec3a43937a740
dineshsoftsuave <p.dinesh@softsuave.com>
2022-03-02 14:36:35 +0530

    Security Config

--- src/main/java/com/example/demo/Registration/RegistrationController.java
+++ src/main/java/com/example/demo/Registration/RegistrationController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/Register")
+@RequestMapping("/api/v1/register")
 public class RegistrationController {
 	
 	@Autowired
---
+++ src/main/java/com/example/demo/security/config/WebSecurityConfig.java
@@ -0,0 +1,55 @@
+package com.example.demo.security.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import com.example.demo.appUser.AppUserService;
+
+import lombok.AllArgsConstructor;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
+	
+	@Autowired
+	private AppUserService userService;
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().disable()
+			.authorizeRequests()
+			.antMatchers("/api/v*/register/**").permitAll()
+			.anyRequest().authenticated()
+			.and()
+			.formLogin();
+	}
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+		provider.setPasswordEncoder(bCryptPasswordEncoder);
+		provider.setUserDetailsService(userService);
+		return provider;
+		
+	}
+
+
+}
